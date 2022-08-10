6af93aea07c36ba4cbc07aa2632d731a5a343e7a
xuhui-web <578734857@qq.com>
2022-08-10 19:49:03 +0800

    first commit

---
+++ commitlint.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  // 继承的规则
+  extends: ['@commitlint/config-conventional'],
+  // 定义规则类型
+  rules: {
+    // type 类型定义，表示 git 提交的 type 必须在以下类型范围内
+    'type-enum': [
+      // 当前验证的错误级别，2代表错误级别的错误
+      2,
+      // 在什么情况下进行验证，always表示任何情况下都进行验证
+      'always',
+      // 泛型内容, 对应cz-config定义的types
+      [
+        'feat', // 新功能 feature
+        'fix', // 修复 bug
+        'docs', // 文档注释
+        'style', // 代码格式(不影响代码运行的变动)
+        'refactor', // 重构(既不增加新功能，也不是修复bug)
+        'perf', // 性能优化
+        'test', // 增加测试
+        'chore', // 构建过程或辅助工具的变动
+        'revert', // 回退
+        'build' // 打包
+      ]
+    ],
+    // subject 大小写不做校验
+    'subject-case': [0]
+  }
+}
